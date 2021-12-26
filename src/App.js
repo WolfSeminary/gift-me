2b59a921264110c2c6f9eefff2d8e2cd868307ea
sari-rozenberg <s0504122749@gmail.com>
2021-12-26 19:08:13 +0200

    makeCardOfGiftDetaild

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import  MultiActionAreaCard from'./GiftDetailed/GiftDetailed';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
+     < MultiActionAreaCard/>
     </div>
   );
 }

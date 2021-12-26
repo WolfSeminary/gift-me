8999750288b7b6b617babf4c9a23db941cc11c0e
michalshveyfel <m2122637@gmail.com>
2021-12-26 16:37:20 +0200

    ButtontoChooseToWhoGift

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import WhoGetTheGift from './WhoGetTheGift.js';
+import './App.css';
+import  React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
-    </div>
+      <WhoGetTheGift/>
   );
 }
 
---
+++ src/WhoGetTheGift.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Typography from '@mui/material/Typography';
+const WhoGetTheGift =()=>{
+return(
+    <>
+    <Typography  variant="h5">
+        <h5>Who gets the gift?</h5>
+        <ToggleButtonGroup onChange={handleChange}>
+            <ToggleButton value="To someone else" >
+            To someone else
+            </ToggleButton>
+            <ToggleButton value="To myself" >
+            To myself
+            </ToggleButton>
+        </ToggleButtonGroup>
+    
+        </Typography>
+    </>
+)
+}
+export default WhoGetTheGift;

f19992e796219ede915c3f414e6a743fbf02bc72
hitmachut <hitmachut@students.local>
2021-12-26 14:47:11 +0200

    addFindGiftButton

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import FindGift from './FindGift';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
+      <FindGift />
     </div>
   );
 }

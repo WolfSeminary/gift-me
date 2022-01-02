735db791a0288f2a5028181c20c39e2a9f7d59d4
tamarRavitz <tamar6765127@gmail.com>
2022-01-02 16:28:36 +0200

    addComponentGift

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Gift from './Gift';
+
 
 function App() {
   return (
     <div className="App">
-      <Gift/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
+        </p>
+      </header>
     </div>
   );
 }

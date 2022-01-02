e41d52e9dc04fe48f520a1ad489464d0aa4e7738
SchneiderRachel <r0534108825@gmail.com>
2022-01-02 17:29:01 +0200

    add categories component with state for rerendering the list category

---
+++ src/Category.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { CardContent, Typography, CardMedia,Card } from '@mui/material';
+
+
+
+const Category=({category})=> {
+    return (
+
+        <Card>
+            <CardMedia
+                component="img"
+                alt={category.name}
+                height="140"
+                image={category.image}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {category.name}
+                </Typography>
+            </CardContent>
+        </Card>
+
+    )
+}
+export default Category;

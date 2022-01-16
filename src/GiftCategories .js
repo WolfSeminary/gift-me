867455e0bd62cc9332ab19f9ba8e478c2f8b8104
hitmachut <hitmachut@students.local>
2022-01-16 17:58:03 +0200

    add component with tag select andclicking on a category will redirect the user to the gifts page

--- src/GiftCategories .js
+++ src/GiftCategories .js
@@ -7,19 +7,38 @@ import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 export default function GiftCategories() {
     const [categoria, setCategoria] = React.useState('');
 
     const handleChange = (event) => {
         setCategoria(event.target.value);
+        
     };
-
+    function onCategoryClick (event){
+        let x=event;
+        console.log(x);
+        <BrowserRouter>
+        <Routes>
+    <Route path="/client/:clientId" component={Client} />
+    {/* //       <Route path="/" element={<App />}>
+    //         <Route index element={<Home />} />
+    //         <Route path="food" element={<Food />}>
+    //           <Route path=":attractions" element={<Attractions />} />
+    //           <Route path="new" element={<GiftCards />} />
+    //           <Route index element={<LeagueStandings />} />
+    //         </Route>
+    //       </Route> */}
+       </Routes>
+       </BrowserRouter>
+    }
     return (
-
-
-
         <div>
-
             <FormControl sx={{ m: 1, minWidth: 120 }}>
                 <Select
                     value={categoria}
@@ -30,10 +49,10 @@ export default function GiftCategories() {
                     <MenuItem value="" disabled hidden>
                         <em>Category</em>
                     </MenuItem>
-                    <MenuItem value={10}>Food</MenuItem>
-                    <MenuItem value={20}>Attractions</MenuItem>
-                    <MenuItem value={30}>Gift Cards</MenuItem>
-                    <MenuItem value={40}>Toys </MenuItem>
+                    <MenuItem value={10}onClick={onCategoryClick("food") }>Food</MenuItem>
+                    <MenuItem value={20}onClick={onCategoryClick("Attractions") }>Attractions</MenuItem>
+                    <MenuItem value={30}onClick={onCategoryClick }>Gift Cards</MenuItem>
+                    <MenuItem value={40}onClick={onCategoryClick }>Toys </MenuItem>
 
                 </Select>
             </FormControl>
@@ -41,3 +60,29 @@ export default function GiftCategories() {
 
     );
 }
+
+
+
+// import { render } from "react-dom";
+// import {
+//     BrowserRouter,
+//     Routes,
+//     Route
+//   } from "react-router-dom";
+  // import your route components too
+  
+//   render(
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />}>
+//           <Route index element={<Home />} />
+//           <Route path="teams" element={<Teams />}>
+//             <Route path=":teamId" element={<Team />} />
+//             <Route path="new" element={<NewTeamForm />} />
+//             <Route index element={<LeagueStandings />} />
+//           </Route>
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>,
+//     document.getElementById("root")
+//   );

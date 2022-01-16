bddb12b644177b8b2b7d0e0cf87a6fc60a096055
michalshveyfel <m212267@gmail.com>
2022-01-16 20:52:19 +0200

    feat:(CategoriesPage)Add new componnent

---
+++ src/CategoriesPage.js
@@ -0,0 +1,14 @@
+import { AppBar } from '@mui/material';
+import React from 'react';
+import Categories from './Categories';
+
+const CategoriesPage = () => {
+   return (
+    <>
+    <AppBar>Gift-me</AppBar>
+    <Categories/>
+    </>
+   )
+}
+
+export default CategoriesPage;

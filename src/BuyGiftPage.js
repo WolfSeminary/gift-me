857e8c2b076b5be43fb52ef88752a8132d3a97af
YehuditRozovsky <r036166146@gmail.com>
2022-01-16 14:42:16 +0200

    add buyGiftPage

---
+++ src/BuyGiftPage.js
@@ -0,0 +1,18 @@
+import AppBar from '/.AppBar';
+import WhoGetTheGift from '/.WhoGetTheGift';
+import GiftRecipient from './GiftRecipient';
+import GiftSender from './GiftSender';
+import GiftNote from './GiftNote';
+
+const BuyGiftPage = () => {
+    return (
+        <>
+            <AppBar />
+            <WhoGetTheGift />
+            <GiftRecipient />
+            <GiftSender />
+            <GiftNote />
+        </>
+    )
+}
+export default BuyGiftPage;

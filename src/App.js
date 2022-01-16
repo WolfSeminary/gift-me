d12d737a7e74d046911c72acde9615f7aeb454c6
michalshveyfel <m2122637@gmail.com>
2022-01-16 12:15:07 +0200

    change App.js and format component BuyGift.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
-
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
+        </p>
+      </header>
     </div>
   );
 }

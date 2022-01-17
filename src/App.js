231ee843e8ddf4b91f29e7d36bd37ba0e22f13d4
shira-tzadok <shira74176@gamil.com>
2022-01-17 18:08:41 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import AppBar from './AppBar'
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
+        </p>
+      </header>    
     </div>
   );
 }

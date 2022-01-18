529220df495ec92ff30beeaa2b7b9ab5ff1a2923
tamarRavitz <tamar6765127@gmail.com>
2022-01-18 15:36:27 +0200

    giftsState

--- src/Categories.js
+++ src/Categories.js
@@ -5,12 +5,10 @@ import Grid from '@mui/material/Grid';
 
 const Categories = () => {    
     const [categories, setCategories] = useState([
-        {id:1,name:'Food',image:"./images/1.jpg"},
-        {id:2,name:'Attractions',image:"./images/1.jpg"},
-        {id:3,name:'Gift Cards',image:"./images/1.jpg"},
-        {id:4,name:'Shopping',image:"./images/1.jpg"},
-        {id:5,name:'Food',image:"./images/1.jpg"},
-        {id:6,name:'Gift Cards',image:"./images/1.jpg"},
+        {id:1,name:'Food',image:"./images/food"},
+        {id:2,name:'Attractions',image:"./images/Attractions"},
+        {id:3,name:'Gift Cards',image:"./images/Gift Cards"},
+        {id:4,name:'Toys',image:"./images/Toys"}
     ]);
     
     return (<>

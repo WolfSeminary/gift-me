6b047fcb0cff8fc6465d4d98c7c0cb3cfb666733
dasiHeizler <dasiheizler@gmail.com>
2022-01-19 20:34:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
 import './App.css';
-import  MultiActionAreaCard from'./GiftDetailed/GiftDetailed';
+import logo from './logo.svg';
+
 function App() {
   return (
     <div className="App">
+       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
+        </p>
+      </header>    
     </div>
   );
 }
--- src/GiftDetailed/GiftDetailed.js
+++ src/GiftDetailed/GiftDetailed.js
@@ -1,128 +1,64 @@
-// import React from 'react';
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
-// //import CardHeader from '@mui/material/CardHeader';
-// import { Button, CardActionArea, CardActions } from '@mui/material';
-
-// export default function MultiActionAreaCard() {
-//   return (
-//     <Card sx={{ maxWidth: 345 }}>
-//       <CardActionArea>
-//         {/* <CardHeader>
-//           <h1>115</h1>
-//         </CardHeader> */}
-//         <CardMedia
-//           component="img"
-//           height="140"
-//           // image="./branch.jpg"
-//           alt="green iguana"
-//         />
-//         <CardContent>
-//           <Typography gutterBottom variant="h5" component="div">
-//            branch
-//           </Typography>
-//           <Typography variant="body2" color="text.secondary">
-//            branch
-//           </Typography>
-//         </CardContent>
-//       </CardActionArea>
-//       <CardActions>
-//         <Button size="big" color="yellow">
-//          choose
-//         </Button>
-//       </CardActions>
-//     </Card>
-//   );
-// }
-// import  React from 'react';
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
-// import { Button, CardActionArea} from '@mui/material';
-
-// import CardHeader from '@mui/material/CardHeader';
-
-
-// export default function MultiActionAreaCard() {
-//   return (
-//     <Card sx={{ maxWidth: 345 }}>
-//       <CardActionArea>
-//       <CardHeader
-
-//         title="Shrimp and Chorizo Paella"
-//         subheader="September 14, 2016"
-//       />
-//         <CardMedia
-//           component="img"
-//           height="140"
-//           image="/branch.JPG"
-//           alt="green iguana"
-//         />
-//         <CardContent>
-//           <Typography gutterBottom variant="h5" component="div">
-//             Lizard
-//           </Typography>
-//           <Typography variant="body2" color="text.secondary">
-//             Lizards are a widespread group of squamate reptiles, with over 6,000
-//             species, ranging across all continents except Antarctica
-//           </Typography>
-//         </CardContent>
-//       </CardActionArea>
-//       <CardActions>
-//         <Button size="big" color="yellow">
-//        choose
-//         </Button>
-//       </CardActions>
-//     </Card>
-//   );
-// }
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import CardHeader from '@mui/material/CardHeader';
-import zIndex from '@mui/material/styles/zIndex';
-import '../GiftDetailed/Giftdetailed.css'
-
-
-
-
-export default function MultiActionAreaCard() {
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActions } from "@mui/material";
+import CardHeader from "@mui/material/CardHeader";
+import "./GiftDetailed.css";
+export default function GiftDetailed() {
   return (
-    <Card sx={{ maxWidth: 250, height: 200 }}>
-
-
-      <CardMedia
-        component="img"
-        height="140"
-        image="/branch.JPG"
-        alt="שניצל"
-      />
-      <CardHeader
-        title={
-          <Typography style={{ position: 'absolute', top: '3vh', left: '43vw', fontSize: '0.7rem' ,borderRadius:'2.5px',width:"8vw"}} gutterBottom bgcolor='white' textAlign='center' variant="h8" mt={-1} component="div">
-            $115
-          </Typography>
-        }
-      />
-      <Card sx={{ width: 250, height: 100 }} style={{ backgroundColor: 'white', position: 'absolute', top: '20vh' }}   >
-        <CardContent  >
-          <Typography gutterBottom  textAlign='center' variant="h7" mt={-1} component="div">
-            branch
-          </Typography>
-        </CardContent>
-        <CardActions >
-          <Button size="small" variant="contained"  style={{ backgroundColor: 'orange' }} fullWidth color="primary">
-            choose
-          </Button>
-        </CardActions>
+    <>
+      <Card sx={{ width: 250, height: 280, position: "relative" }}>
+        <CardMedia
+          component="img"
+          height="200"
+          image="/branch.JPG"
+          alt="שניצל"
+        />
+        <CardHeader
+          title={
+            <Typography
+              sx={{
+                top: "3vh",
+                left: "8.5vw",
+                fontSize: "0.7rem",
+                borderRadius: "2.5px",
+                position: "absolute",
+                width: "3vw",
+              }}
+              gutterBottom
+              bgcolor="white"
+              textAlign="center"
+              variant="h8"
+              mt={-1}
+              component="div"
+            >
+              $115
+            </Typography>
+          }
+        />
+        <Card
+          sx={{ width: 250, height: 120, position: "absolute", top: "18vh" }}
+        >
+          <CardContent>
+            <Typography
+              gutterBottom
+              textAlign="center"
+              variant="h7"
+              mt={-1}
+              component="div"
+            >
+              branch
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button variant="contained" className="button" fullWidth>
+              choose
+            </Button>
+          </CardActions>
+        </Card>
       </Card>
-
-    </Card>
+    </>
   );
 }

f4fcd7c047bab88da09e22138e7b8cee33a4726e
MiryamRaived <mirir6443@gmail.com>
2022-01-23 14:04:23 +0200

    aaa

--- src/Gifts.js
+++ src/Gifts.js
@@ -1,18 +1,35 @@
 import Gift from "./Gift";
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { useParams } from 'react-router-dom';
 
- const Gifts = () => {
-     let id=1;
+
+export default function Gifts() {
+    let { id: categoryId } = useParams();
     const navigate = useNavigate();
     const onGiftClick = () => {
-        navigate(`gift/${id}`);
+        navigate(`gift/${categoryId}`);
     }
+    const [gifts, setGifts] = useState([
+        { id: 1, name: "gift1", image: "./SSGP0149.JPG" },
+        { id: 2, name: "gift2", image: "./SSGP0149.JPG" },
+        { id: 3, name: "Gift Card", image: "./SSGP0149.JPG" },
+        { id: 4, name: "gift3", image: "./SSGP0149.JPG" },
+    ]);
+    console.log(gifts)
     return (
-        <>
-            <div onClick={onGiftClick}>
-                <Gift />
-            </div>
-        </>
+        <Box sx={{ flexGrow: 1, margin: 3 }}>
+            <Grid container spacing={2}>
+                {gifts.filter(item => item.categoryID == categoryId || categoryId == null).map((gift) => (
+                    <Grid gift xs={6} key={gift.id} textAlign={"center"} md={6}>
+                        <div onClick={onGiftClick}>
+                            <Gift key={gift.id} myGift={gift} />
+                        </div>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
     )
 }
-export default Gifts;

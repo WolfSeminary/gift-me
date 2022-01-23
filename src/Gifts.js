bc3b8d1933b5bc17d0c6048ab2865fdefcfa9688
MiryamRaived <mirir6443@gmail.com>
2022-01-23 14:47:06 +0200

    bbb

--- src/Gifts.js
+++ src/Gifts.js
@@ -5,18 +5,17 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { useParams } from 'react-router-dom';
 
-
-export default function Gifts() {
+export default function Gifts() {   
     let { id: categoryId } = useParams();
     const navigate = useNavigate();
     const onGiftClick = () => {
-        navigate(`gift/${categoryId}`);
+        navigate(`gifts/${categoryId}`);
     }
     const [gifts, setGifts] = useState([
-        { id: 1, name: "gift1", image: "./SSGP0149.JPG" },
-        { id: 2, name: "gift2", image: "./SSGP0149.JPG" },
-        { id: 3, name: "Gift Card", image: "./SSGP0149.JPG" },
-        { id: 4, name: "gift3", image: "./SSGP0149.JPG" },
+        { id: 1,categoryID:1, name: "gift1", image: "./SSGP0149.JPG" },
+        { id: 2,categoryID:1, name: "gift2", image: "./SSGP0149.JPG" },
+        { id: 3,categoryID:1, name: "Gift", image: "./SSGP0149.JPG" },
+        { id: 4,categoryID:1, name: "gift3", image: "./SSGP0149.JPG" },
     ]);
     console.log(gifts)
     return (

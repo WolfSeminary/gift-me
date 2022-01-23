af6f83c20c03631975f6390d255f2d0e17b95403
Daniela-Sharabi <d798659@gmail.com>
2022-01-23 12:01:27 +0200

    addanewgiftcomponent

--- src/Gift.js
+++ src/Gift.js
@@ -1,28 +1,32 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
-const Gift=()=> {
+const Gift = ({ myGift }) => {
+  console.log(myGift)
   return (
     <Card sx={{ maxWidth: 345 }}>
-    
-        <CardMedia
-          component="img"
-          height="200"
-          image="./SSGP0149.JPG"
-          alt="picture"
-        />
-        <CardContent >
-            <div></div>
-          <Typography borderRadius="16%"  sx={{ borderRadius: 16}} gutterBottom variant="h5" component="div">
-            BRUNCH
-          </Typography>
-        </CardContent>
-      
+      <CardMedia
+        component="img"
+        height="200"
+        image={myGift.image}
+        alt="picture"
+      />
+      <CardContent>
+        <div></div>
+        <Typography
+          borderRadius="16%"
+          sx={{ borderRadius: 16 }}
+          gutterBottom
+          variant="h5"
+          component="div"
+        >
+          {myGift.name}
+        </Typography>
+      </CardContent>
     </Card>
   );
-}
+};
 export default Gift;
---
+++ src/Gifts.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Gift from "./Gift";
+
+export default function Gifts() {
+    const [gifts, setGifts] = useState([
+        { id: 1, name: "gift1", image: "./SSGP0149.JPG" },
+        { id: 2, name: "gift2", image: "./SSGP0149.JPG" },
+        { id: 3, name: "Gift Card", image: "./SSGP0149.JPG" },
+        { id: 4, name: "gift3", image: "./SSGP0149.JPG" },
+    ]);
+    console.log(gifts)
+    return (
+        <Box sx={{ flexGrow: 1, margin: 3 }}>
+            <Grid container spacing={2}>
+                {gifts.map((gift) => (
+                    <Grid gift xs={6} key={gift.id} textAlign={"center"} md={6}>
+                        <Gift key={gift.id} myGift={gift} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
+}

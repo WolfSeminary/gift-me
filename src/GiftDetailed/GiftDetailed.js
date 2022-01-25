4b10fd13526958e65057999839be28edc9ccd836
sari-rozenberg <96005497+sari-rozenberg@users.noreply.github.com>
2022-01-25 20:07:03 +0200

    feat(GiftDetaild):make  card of gift detaild (#19)
    * makeCardOfGiftDetaild
    
    * makeCardOfGiftDetaild
    
    * update
    
    * conflict
    
    Co-authored-by: dasiHeizler <dasiheizler@gmail.com>

---
+++ src/GiftDetailed/GiftDetailed.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActions } from "@mui/material";
+import CardHeader from "@mui/material/CardHeader";
+import "./GiftDetailed.css";
+export default function GiftDetailed() {
+  return (
+    <>
+      <Card sx={{ width: 250, height: 280, position: "relative" }}>
+        <CardMedia
+          component="img"
+          height="200"
+          image="/branch.JPG"
+          alt="שניצל"
+        />
+        <CardHeader
+          title={
+            <Typography
+              sx={{
+                top: "3vh",
+                left: "8.5vw",
+                fontSize: "0.7rem",
+                borderRadius: "2.5px",
+                position: "absolute",
+                width: "3vw",
+              }}
+              gutterBottom
+              bgcolor="white"
+              textAlign="center"
+              variant="h8"
+              mt={-1}
+              component="div"
+            >
+              $115
+            </Typography>
+          }
+        />
+        <Card
+          sx={{ width: 250, height: 120, position: "absolute", top: "18vh" }}
+        >
+          <CardContent>
+            <Typography
+              gutterBottom
+              textAlign="center"
+              variant="h7"
+              mt={-1}
+              component="div"
+            >
+              branch
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button variant="contained" className="button" fullWidth>
+              choose
+            </Button>
+          </CardActions>
+        </Card>
+      </Card>
+    </>
+  );
+}

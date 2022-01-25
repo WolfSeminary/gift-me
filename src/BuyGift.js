ff6a2dd5574cd086baad4f79db698ce311a87379
YehuditRozovsky <r036166146@gmail.com>
2022-01-25 15:30:18 +0200

    Invoke the Buy Gift button & Show the gift preview

--- src/BuyGift.js
+++ src/BuyGift.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+import GiftPreview from './GiftPreview';
 
-const BuyGift = () => {
+const BuyGift = ({info}) => {
+   const [shouldShowPreview, setShouldShowPreview] = useState(false);
+   function onBuyGiftClick() {
+      setShouldShowPreview(true);
+   }
    return (
-      <Button variant="contained" disableElevation>
-         Buy Gift
-      </Button>
+      <>
+         <Button variant="contained" disableElevation onClick={onBuyGiftClick}>
+            Buy Gift
+         </Button>
+         {shouldShowPreview && <GiftPreview info={info}/>}
+      </>
    )
 }
 
---
+++ src/GiftPreview.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const GiftPreview = ({ info }) => {
+    return (<>
+        {info.id}
+        {info.name}
+        {info.image}
+    </>
+    )
+}
+export default GiftPreview;

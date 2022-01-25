b790330f352ea408e80ed135d2d8c9cd01d06a48
shira-tzadok <shira74176@gamil.com>
2022-01-25 19:38:31 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
-import Button from './Button';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-       {/* <header className="App-header">
+       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Deploy-work!!
         </p>
-      </header>     */}
-      <Button/>
+      </header>    
     </div>
   );
 }

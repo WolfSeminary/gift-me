d4371acb29193b82058c70d8e5bb2290e01e4321
shira-tzadok <96005355+shira-tzadok@users.noreply.github.com>
2022-01-25 19:44:48 +0200

    createFindGiftButton (#72)
    * createFindGiftButton
    
    * update
    
    Co-authored-by: shira-tzadok <shira74176@gamil.com>

---
+++ src/FindGift.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const FindGift = () => {
+    return <Button variant="contained">Find a Gift</Button>
+}
+export default FindGift

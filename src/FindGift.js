4be9d412bd70c3ad9dfce532573026a04cb29e6a
Daniela-Sharabi <96005511+Daniela-Sharabi@users.noreply.github.com>
2022-01-28 00:01:04 +0200

    feat(ClickingTheFindGiftButton):add clicking the findGift button should redirect to the categories page (#73)

--- src/FindGift.js
+++ src/FindGift.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React from "react";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 const FindGift = () => {
-    return <Button variant="contained">Find a Gift</Button>
-}
-export default FindGift
+  const navigate = useNavigate();
+
+  return (
+    <Button
+      variant="contained"
+      onClick={() => {
+        navigate(`categories`);
+      }}
+    >
+      Find a Gift
+    </Button>
+  );
+};
+export default FindGift;

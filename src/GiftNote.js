70d199deb4625373453ac7f46ad566de149f4a8f
Daniela-Sharabi <96005511+Daniela-Sharabi@users.noreply.github.com>
2022-01-30 01:20:26 +0200

    feat(onChangeProps):onchange props in components (#69)
    * onChengeProps
    
    * fix
    
    * try
    
    * fix
    
    Co-authored-by: sari <s0504122749@gmail.com>

--- src/GiftNote.js
+++ src/GiftNote.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
-const GiftNote = ({ giftNote }) => {
-  return (<>
-
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Gift Note" variant="outlined" value={giftNote} multiline rows={5} style={{ width: 600 }} fullWidth />
-    </Box>
-  </>)
-}
-export default GiftNote
+const GiftNote = ({ onChange, value }) => {
+ 
+  return (
+    <>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          value={value}
+          onChange={onChange}
+          id="outlined-basic"
+          label="Gift Note"
+          variant="outlined"
+          multiline
+          rows={5}
+          style={{ width: 600 }}
+          fullWidth
+        />
+      </Box>
+    </>
+  );
+};
+export default GiftNote;
--- src/WhoGetTheGift.js
+++ src/WhoGetTheGift.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import Typography from "@mui/material/Typography";
 
-const WhoGetTheGift = ({ whoGetTheGift }) => {
-    return (
-        <>
-            <Typography variant="h5">Who gets the gift?</Typography>
-            <ToggleButtonGroup >
-                <ToggleButton value={whoGetTheGift} >
-                    A Friend
-                </ToggleButton>
-                <ToggleButton value={whoGetTheGift} >
-                    To myself
-                </ToggleButton>
-            </ToggleButtonGroup>
-        </>
-    )
-}
+const WhoGetTheGift = ({ onChange, value }) => {
+  return (
+    <>
+      <Typography variant="h5">Who gets the gift?</Typography>
+      <ToggleButtonGroup value={value} onChange={onChange}>
+        <ToggleButton value="To someone else">To someone else</ToggleButton>
+        <ToggleButton value="To myself">To myself</ToggleButton>
+      </ToggleButtonGroup>
+    </>
+  );
+};
 
 export default WhoGetTheGift;

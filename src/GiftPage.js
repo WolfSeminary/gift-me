e56d2602c0f93c1d03775f05ae7e4ba1a8a13763
YehuditRozovsky <96005532+YehuditRozovsky@users.noreply.github.com>
2022-02-06 14:35:09 +0200

    feat(GiftPage):Add gift page component (#50)
    * add gift page
    
    * add import to components
    
    * "change breadcrumbs to navigation"

---
+++ src/GiftPage.js
@@ -0,0 +1,14 @@
+import AppBar from './AppBar';
+import Navigation from './Navigation';
+import GiftDetailed from './GiftDetailed';
+
+const GiftPage = () => {
+    return (
+        <>
+            <AppBar />
+            <Navigation />
+            <GiftDetailed />
+        </>
+    )
+}
+export default GiftPage;

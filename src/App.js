e7ed438c3dea3186fbcdabc5e3795482f2d88679
YehuditRozovsky <r036166146@gmail.com>
2022-02-06 11:08:36 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
-import BuyGift from './BuyGift';
 import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-       {/* <header className="App-header">
+       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Deploy-work!!
         </p>
-      </header>     */}
-      <BuyGift/>
+      </header>    
     </div>
   );
 }
